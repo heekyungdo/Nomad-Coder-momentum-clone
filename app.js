e7e7b44140e0a94cddc6335d8378cc9827c7d37b
Heekyung Do <dhk0729@gmail.com>
2021-09-30 22:09:47 +0900

    first commit

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
+// const loginForm = document.getElementById("login-form");
+
 const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
+
+// 클릭 이벤트는 loginButton과 연결되어 있어야돼
+// loginButton에 대한 click을 감지
+function onLoginBtnClick() {
+  const username = loginInput.value;
+  console.log(username);
+}
+
+loginButton.addEventListener("cilck", onLoginBtnClick);
