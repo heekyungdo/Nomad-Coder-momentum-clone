437988dec1ff0a2828c188102e78e2540e8857d7
Heekyung Do <dhk0729@gmail.com>
2021-10-05 23:07:58 +0900

    Momentum LogIn

--- app.js
+++ app.js
@@ -1,100 +1,44 @@
-// HTML에서 id=login-form을 찾고 있어.
-// JS가 loginForm을 찾고 있다면, loginForm은 HTML 내에 있는 element라는 뜻.
-// 그 element는 바로 <div id="login-form"></div>야.
-// const loginForm = document.getElementById("login-form");
-// document에서 찾는 대신에 const loginForm에서 찾을거야.
-// const loginInput = loginForm.querySelector("input");
-// const loginButton = loginForm.querySelector("button");
-
-// 위 세 코드를 짧고, 쉽게 바꾼 것
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
-// string만 포함된 변수는 대문자로 표기하고, string을 저장하고 싶을 때 대문자 사용
+
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
-// const link = document.querySelector("a");
-// const loginButton = document.querySelector("#login-form button");
 
-// function onLoginBtnClick() {
-// 이렇게 쓰면 <input tyle="text" placeholder="What is your name?">가 console에 입력됌.
-// HTML과 같은 트리 구조로 출력
-// console.log("hello", loginInput.value);
-// 이렇게 쓰면 loginInput의 내부를 보여줘. JSON과 같은 트리구조로 출력
-// console.dir(loginInput);
-// console.log("click!!");
-
-// const username = loginInput.value;
-// if (username === "") {
-//   alert("Please write your name.");
-// } else if (username.length > 15) {
-//   alert("Your name is too long.");
-// }
-// console.log(username);
-// }
-// 클릭 이벤트는 loginButton과 연결되어 있어야돼
-// loginButton에 대한 click을 감지
-// loginButton.addEventListener("click", onLoginBtnClick);
-
-// submit event 감지
-// 새로고침 일어나는 건 form submit의 기본 동작.
+// submit 이벤트 리스너에 의해 함수 실행
 function onLoginSubmit(event) {
-  // preventDefault는 어떤 event의 기본 행동이 발생되지 않도록 막는것.
-  // 입력값은 저장하고
+  // submit의 default인 새로고침을 없앤다.
   event.preventDefault();
+  //class"hidden"을 다시 더하여 form을 보이지 않게 한다.
   loginForm.classList.add(HIDDEN_CLASSNAME);
+  // input text의 value를 usernameThatTheUwerWorte 변수에 저장한다.
   const usernameThatTheUserWrote = loginInput.value;
+  // local storage에 key:value를 저장한다. (key=username , value=usernameThatTheUserWorte)
   localStorage.setItem(USERNAME_KEY, usernameThatTheUserWrote);
-  // user의 이름을 변수로 저장해주고, 그 이름은 h1 안에 넣어준다.
-  // string과 변수를 하나로 합치는 두가지 방법 (아래 두줄)
-  // greeting.innerText = "Hello " + usernameThatTheUserWrote;
-
-  // paintGreetings함수는 usernameTHatTheUwerWorte의 인자값을 가져온다.
-  // input의 value값을 인자로 넣어준다.
+  // 유저 정보가 input으로 부터 온다.
   paintGreetings(usernameThatTheUserWrote);
-  // hidden이라는 class name을 더해줘서 form을 숨기고
-  console.log(usernameThatTheUserWrote);
-  // console.log(loginInput.value);
-  // const username = loginInput.value;
-  // console.log(username);
 }
 
-// ()을 더하면 브라우저가 보자마자 자동으로 이 function을 실행시킬거야.
-// onLoginSubmit();
-
-// function handleLinkClick(event) {
-//   event.preventDefault();
-//   console.dir(event);
-// alert이 이 page가 다른 동작을 하지 못하도록 막고 있어. 확인 버튼을 누른 이후엔
-// default값이 실행된다.
-// alert("clicked!");
-// }
-
-// link.addEventListener("click", handleLinkClick);
-
-// JS는 함수를 실행시키는 동시에 그 함수에 첫번째 인자로 object를 넣어 줄것임.
-// 그리고 object에는 방금 일어난 event에 대한 여러 정보가 담겨있다.
-// handleLinkClick({information about the event that just happened});
-
+// onLoginSubmit 함수와 else의 코드가 중복되니 새로운 함수 생성하여 코드 중복되지 않게 한다.
+// usernameThatTheUserWrote을 인자로 받는 함수
+// 우리가 함수를 호출하는 위치에 따라 유저 정보는 다른 곳에서 오게 할거야
 function paintGreetings(usernameThatTheUserWrote) {
-  // const username = localStorage.getItem(USERNAME_KEY);
-  // 이름을 추가한 다음에
-  greeting.innerText = `Hello ${usernameThatTheUserWrote}!`;
-  // greeing한테서 HIDDEN_CLASSNAME을 제거해주기
+  greeting.innerText = `Hello, ${usernameThatTheUserWrote}!`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-// local storage에서 username이라는 키를 가져와
+// localStorage에 key값이 있는지 확인하기 위해 key값 가져온다
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
-// if savedUsername = null
+// 만약 localStorage에 key와 value가 없어 null인 상태면
 if (savedUsername === null) {
-  // show the form
+  // form을 보여준다. class="hidden"을 없앰으로서
   loginForm.classList.remove(HIDDEN_CLASSNAME);
-  // addEventListener는 브라우저가 바로 실행시키려는게 아니라 submit을 해야 실행된다.
+  // form에 있는 submit이 이벤트리슨되면, onLoginSubmit 함수 실행
   loginForm.addEventListener("submit", onLoginSubmit);
-  // 그렇지 않으면, show the greetings
+  // if값이 거짓이면 실행
 } else {
-  // paintGreetings 함수는 위와 달리 local storage로부터유저 정보를 받는다.
+  // 유저 정보가 local storage로 부터 온다.
+  // local storage에 유저 정보가 있으면 거기서 유저 정보를 받아서 인자로 넣어준다.
   paintGreetings(savedUsername);
 }
