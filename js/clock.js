4ed104055ec0b40d37b0d6378e8eca80e4bf7cd3
Heekyung Do <dhk0729@gmail.com>
2021-10-06 22:28:22 +0900

    started quotes part

--- js/clock.js
+++ js/clock.js
@@ -1,6 +1,7 @@
 const clock = document.querySelector("h2#clock");
 
 function getClock() {
+  // 호출하는 당시의 현재 날짜랑 시간을 알려준다.
   const date = new Date();
   // getHours,getMinutes.getSeconds는 number라서 문자로 바꿔준다.
   // "1".padStart(2,"0") -> string의 길이를 2로 만드는데, 그 string의 길이가 2가 아니라면 앞쪽에 "0"을 추가한다.
@@ -23,5 +24,8 @@ function getClock() {
 // sayHello 함수는 5초(5000ms)후에 호출
 // setTimeout(sayHello, 5000);
 
+// 브라우저가 처음 실행될 때 바로 시간을 보여준다.
+// 브라우저가 처음 실행될 때 이 함수가 실행되지 않으면 제일 처음에는 00:00:00이 보여진다.
 getClock();
+// 시간을 1초단위로 보여준다.
 setInterval(getClock, 1000);
---
+++ js/quotes.js
@@ -0,0 +1,74 @@
+const quotes = [
+  {
+    quote:
+      "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+    author: "Winston Churchill",
+  },
+  {
+    quote: "A problem well stated is a problem half-solved.",
+    author: "Charles Kettering",
+  },
+  {
+    quote:
+      "Knowing is not understanding. There is a great difference between knowing and understanding: you can know a lot about something and not really understand it.",
+    author: "Charles Kettering",
+  },
+  {
+    quote:
+      "If at first you don't succeed, try, try again. Then quit. There's no point in being a damn fool about it.",
+    author: "W. C. Fields",
+  },
+  {
+    quote: "People may hear your words, but they feel your attitude.",
+    author: "John C. Maxwell",
+  },
+  {
+    quote: "Attitude determines the altitude of life.",
+    author: "Edwin Louis Cole",
+  },
+  {
+    quote:
+      "Acting is magical. Change your look and your attitude, and you can be anyone.",
+    author: "Alicia Witt",
+  },
+  {
+    quote:
+      "The attitude is very important. Because, your behavior radiates how you feel.",
+    author: "Lou Ferrigno",
+  },
+  {
+    quote: "I will do me, you do you.",
+    author: "Heekyung Do",
+  },
+  {
+    quote:
+      "Keep your face always toward the sunshine - and shadows will fall behind you.",
+    author: "Walt Whitman",
+  },
+  {
+    quote: "Change your thoughts and you change your world.",
+    author: "Norman Vincent Peale",
+  },
+  {
+    quote: "Beauty is power; a smile is its sword.",
+    author: "John Ray",
+  },
+  {
+    quote:
+      "Character contributes to beauty. It fortifies a woman as her youth fades. A mode of conduct, a standard of courage, discipline, fortitude, and integrity can do a great deal to make a woman beautiful.",
+    author: "Jacqueline Bisset",
+  },
+  {
+    quote: "Life is 10% what happens to you and 90% how you react to it.",
+    author: "Charles R. Swindoll",
+  },
+  {
+    quote: "Set your goals high, and don't stop till you get there.",
+    author: "Bo Jackson",
+  },
+];
+
+const quote = document.querySelector("#quote span:first-child");
+const author = document.querySelector("#quote span:last-child");
+
+console.log(quotes[0]);
